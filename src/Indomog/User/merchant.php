d6ba8ce81c7f8a6e1d26f1c1f83a1c9bdad7edfd
biyono <biyono@indomog.com>
2014-04-30 17:21:06 +0700

    merchant add setter
    Signed-off-by: biyono <biyono@indomog.com>

--- src/Indomog/User/merchant.php
+++ src/Indomog/User/merchant.php
@@ -7,4 +7,23 @@ class Merchant
 	private $name;
 
 	private $email;
+
+	public function setId($id)
+	{
+		$this->id=$id;
+		return $this;
+	}
+
+	public function setName($name)
+	{
+		$this->name=$name;
+		return $this;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email=$email;
+		return $this;
+	}
+
 }

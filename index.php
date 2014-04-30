b37ba3441cbbcc689cb63fd9907289bc68a8cc6e
biyono <biyono@indomog.com>
2014-04-30 17:25:55 +0700

    file index add Merchant set
    Signed-off-by: biyono <biyono@indomog.com>

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
+
+use Indomog\User\merchant.php;
+
+$merchant=new Merchant();
+$merchant->setId(1)
+		 ->setName('biyono')
+		 ->setEmail('biyono@indomog.com');
 
 ?>

4ad59516b46696c39f95cab8e3b2089360aabbe3
Sonny <sonny@indomog.com>
2014-04-30 17:34:20 +0700

    Update script
    Signed-off-by: Sonny <sonny@indomog.com>

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
+use Indomog\User\member;
+
+$user = new member();
+
+$user->setId(1);
+$user->setName('sonny');
+$user->setEmail ('sonny@indomog.com');
+
+
+echo '<pre>', var_dump($user), '</pre>';
 
 ?>

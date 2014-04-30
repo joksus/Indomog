a48b4642356df00ee7c3c5db8fbba656748eb9f0
Sonny <sonny@indomog.com>
2014-04-30 17:23:21 +0700

    Update member.php script
    Signed-off-by: Sonny <sonny@indomog.com>

--- src/Indomog/User/member.php
+++ src/Indomog/User/member.php
@@ -7,4 +7,43 @@ class Member
 	private $name;
 
 	private $email;
+
+	public function getid()
+	{
+		return $this->id;
+	}
+
+	public function getname()
+	{
+		return $this->name;
+	}
+
+	public function getemail()
+	{
+		return $this->email;
+	}
+
+	public function setid()
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	public function setid()
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	public function setname()
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+	public function setemail()
+	{
+		$this->email = $email;
+		return $this;
+	}
 }

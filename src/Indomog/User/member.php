f168f9e55a2f2d7388a640f85463d0526c3c619f
Joko Susanto <joko.susanto@indomog.com>
2014-04-30 17:46:44 +0700

    Resolve bug
    Signed-off-by: Joko Susanto <joko.susanto@indomog.com>

--- src/Indomog/User/member.php
+++ src/Indomog/User/member.php
@@ -9,19 +9,19 @@ class Member
 
 	private $email;
 
-	public function setId()
+	public function setId($id)
 	{
 		$this->id = $id;
 		return $this;
 	}
 
-	public function setName()
+	public function setName($name)
 	{
 		$this->name = $name;
 		return $this;
 	}
 
-	public function setEmail()
+	public function setEmail($email)
 	{
 		$this->email = $email;
 		return $this;
